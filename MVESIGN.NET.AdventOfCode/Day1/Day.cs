7c4a0d18ede7b40ca99889e1d3f0ea74494fc484
mvesign <git@mvesign.com>
2016-08-12 21:42:35 +0200

    Added functionalities for Day 1
    Added functionalities for Day 1

---
+++ MVESIGN.NET.AdventOfCode/Day1/Day.cs
@@ -0,0 +1,38 @@
+﻿using MoreLinq;
+using System;
+using System.Linq;
+
+namespace MVESIGN.NET.AdventOfCode.Day1
+{
+    /// <summary>
+    /// Class containing functionalities for the current day.
+    /// </summary>
+    public class Day : MVESIGN.NET.AdventOfCode.Day
+    {
+        /// <summary>
+        /// Create an instance of the current day.
+        /// </summary>
+        public Day()
+            : base(1)
+        {
+            FileContent = CurrentDay.ReadAllText();
+        }
+
+        /// <summary>
+        /// Process the current day.
+        /// </summary>
+        public override void Process()
+        {
+            // Part one
+            Console.WriteLine("Part 1: " + FileContent.Sum(character => character == '(' ? 1 : -1));
+
+            // Part two
+            Console.WriteLine("Part 2: " + FileContent
+                .Scan(0, (f, d) => d == '(' ? f + 1 : f - 1)
+                .Select((floor, index) => new { floor, index })
+                .First(f => f.floor == -1)
+                .index
+            );
+        }
+    }
+}
