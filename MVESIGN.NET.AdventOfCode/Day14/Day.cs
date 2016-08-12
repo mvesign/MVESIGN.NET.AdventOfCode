bc7f3b4f142652c411c0045b8510a553a57a8e3d
mvesign <git@mvesign.com>
2016-08-12 21:44:57 +0200

    Added functionalities for Day 14
    Added functionalities for Day 14

---
+++ MVESIGN.NET.AdventOfCode/Day14/Day.cs
@@ -0,0 +1,116 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MVESIGN.NET.AdventOfCode.Day14
+{
+    /// <summary>
+    /// Class containing functionalities for the current day.
+    /// </summary>
+    public class Day : MVESIGN.NET.AdventOfCode.Day
+    {
+        /// <summary>
+        /// Create an instance of the current day.
+        /// </summary>
+        public Day()
+            : base(14)
+        {
+            FileLines = CurrentDay.ReadAllLines();
+        }
+
+        /// <summary>
+        /// List of active reindeers.
+        /// </summary>
+        public List<Reindeer> Reindeers { get; set; }
+
+        /// <summary>
+        /// Process the current day.
+        /// </summary>
+        public override void Process()
+        {
+            Reindeers = convertToReindeers();
+
+            processRace(2503);
+
+            // Part one
+            Console.WriteLine(string.Format("Part 1: {0}", selectLeaderOfRace()));
+
+            // Part two
+            Console.WriteLine(string.Format("Part 2: {0}", Reindeers.MaxBy(reindeer => reindeer.Points)));
+        }
+
+        /// <summary>
+        /// Calculate the next position for a given reindeer.
+        /// </summary>
+        /// <param name="reindeer">Details of the reindeer.</param>
+        private void calculateNextPosition(Reindeer reindeer)
+        {
+            if (reindeer.IsResting && reindeer.SecondsInPeriod >= reindeer.RestingPeriod)
+            {
+                reindeer.SwapPeriod();
+            }
+
+            if (!reindeer.IsResting)
+            {
+                if (reindeer.SecondsInPeriod >= reindeer.TravelPeriod)
+                {
+                    reindeer.SwapPeriod();
+                }
+                else
+                {
+                    reindeer.TraveledFor += reindeer.TravelDistance;
+                }
+            }
+
+            reindeer.SecondsInPeriod++;
+        }
+
+        /// <summary>
+        /// Convert the current set of file lines into reindeers.
+        /// </summary>
+        /// <returns>Returns the converted set of reindeers.</returns>
+        private List<Reindeer> convertToReindeers()
+        {
+            return FileLines.Select(fileLine =>
+                    Regex.Match(fileLine, @"(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.")
+                        .Groups.Cast<Group>()
+                        .Skip(1)
+                        .Select(group => group.Value)
+                        .ToArray()
+                )
+                .Select(reindeer => new Reindeer()
+                {
+                    Name = reindeer[0],
+                    RestingPeriod = int.Parse(reindeer[3]),
+                    TravelDistance = int.Parse(reindeer[1]),
+                    TravelPeriod = int.Parse(reindeer[2])
+                })
+                .ToList();
+        }
+
+        /// <summary>
+        /// Process the reindeer race.
+        /// </summary>
+        /// <param name="seconds">Number of seconds the race is in progress.</param>
+        private void processRace(int seconds)
+        {
+            for (int period = 0; period < seconds; period++)
+            {
+                Reindeers.ForEach(reindeer => calculateNextPosition(reindeer));
+
+                selectLeaderOfRace().Points++;
+            }
+        }
+
+        /// <summary>
+        /// Select the leading reindeer of the race in the current state.
+        /// </summary>
+        /// <returns>Returns the selected reindeer.</returns>
+        private Reindeer selectLeaderOfRace()
+        {
+            return Reindeers.MaxBy(reindeer => reindeer.TraveledFor);
+        }
+    }
+}
---
+++ MVESIGN.NET.AdventOfCode/Day14/Reindeer.cs
@@ -0,0 +1,66 @@
+﻿namespace MVESIGN.NET.AdventOfCode.Day14
+{
+    /// <summary>
+    /// Class containing details of Santa's reindeer.
+    /// </summary>
+    public class Reindeer
+    {
+        /// <summary>
+        /// Whether the reindeer is resting or not.
+        /// </summary>
+        public bool IsResting { get; set; }
+
+        /// <summary>
+        /// Name of the reindeer.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Number of points rewarded to the reindeer.
+        /// </summary>
+        public int Points { get; set; }
+
+        /// <summary>
+        /// Number of seconds the reindeer needs to rest.
+        /// </summary>
+        public int RestingPeriod { get; set; }
+
+        /// <summary>
+        /// Number of seconds the reindeer is within the current period.
+        /// </summary>
+        public int SecondsInPeriod { get; set; }
+
+        /// <summary>
+        /// Number of kilometers the reindeer can travel per second.
+        /// </summary>
+        public int TravelDistance { get; set; }
+
+        /// <summary>
+        /// Number of total traveled distance.
+        /// </summary>
+        public int TraveledFor { get; set; }
+
+        /// <summary>
+        /// Number of seconds the reindeer can travel.
+        /// </summary>
+        public int TravelPeriod { get; set; }
+
+        /// <summary>
+        /// Swap the current period of the reindeer.
+        /// </summary>
+        public void SwapPeriod()
+        {
+            IsResting = !IsResting;
+            SecondsInPeriod = 0;
+        }
+
+        /// <summary>
+        /// Convert a string that represents the current object.
+        /// </summary>
+        /// <returns>Returns a string that represents the current object.</returns>
+        public override string ToString()
+        {
+            return string.Format("{0} has travelled {1} km with a total of {2} points", Name, TraveledFor, Points);
+        }
+    }
+}
