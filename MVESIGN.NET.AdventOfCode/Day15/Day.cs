e3bf79cac1e65d7c1a8bc922762269b755b52082
mvesign <git@mvesign.com>
2016-08-12 21:45:05 +0200

    Added functionalities for Day 15
    Added functionalities for Day 15

---
+++ MVESIGN.NET.AdventOfCode/Day15/Day.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MVESIGN.NET.AdventOfCode.Day15
+{
+    /// <summary>
+    /// Class containing functionalities for the current day.
+    /// </summary>
+    public class Day : MVESIGN.NET.AdventOfCode.Day
+    {
+        /// <summary>
+        /// Create an instance of the current day.
+        /// </summary>
+        public Day()
+            : base(15)
+        {
+            FileLines = CurrentDay.ReadAllLines();
+        }
+
+        /// <summary>
+        /// Process the current day.
+        /// </summary>
+        public override void Process()
+        {
+            List<Ingredient> ingredients = convertToIngredients();
+
+            var scores = distribute(new int[ingredients.Count], 100, 0)
+                   .Select(amounts => calculateCookieScore(ingredients, amounts))
+                   .ToList();
+
+            // Part one
+            Console.WriteLine(string.Format("Part 1: {0}", scores.Max(score => score.Item1)));
+
+            // Part two
+            Console.WriteLine(string.Format("Part 2: {0}", scores.Where(score => score.Item2 == 500).Max(score => score.Item1)));
+        }
+
+        /// <summary>
+        /// Calculate the score of each cookie ingredient list.
+        /// </summary>
+        /// <param name="ingredients">List of ingredients.</param>
+        /// <param name="amounts">List of amount of teaspoons per ingredient.</param>
+        /// <returns>Returs the calculated cookie score.</returns>
+        private Tuple<long, long> calculateCookieScore(List<Ingredient> ingredients, int[] amounts)
+        {
+            var score = ingredients
+                .Zip(amounts, (ingredient, amount) => ingredient * amount)
+                .Aggregate((a, b) => a + b);
+
+            return Tuple.Create(score.Score, score.Calories);
+        }
+
+        /// <summary>
+        /// Convert the current set of file lines into ingredients.
+        /// </summary>
+        /// <returns>Returns the converted set of ingredients.</returns>
+        private List<Ingredient> convertToIngredients()
+        {
+            return FileLines.Select(fileLine =>
+                    Regex.Match(fileLine, @"(\w+): capacity ([\-]?\d+), durability ([\-]?\d+), flavor ([\-]?\d+), texture ([\-]?\d+), calories ([\-]?\d+)")
+                        .Groups.Cast<Group>()
+                        .Skip(1)
+                        .Select(group => group.Value)
+                        .ToArray()
+                )
+                .Select(ingredient => new Ingredient()
+                {
+                    Capacity = int.Parse(ingredient[1]),
+                    Calories = int.Parse(ingredient[5]),
+                    Durability = int.Parse(ingredient[2]),
+                    Flavor = int.Parse(ingredient[3]),
+                    Texture = int.Parse(ingredient[4]),
+                })
+                .ToList();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="target"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        private IEnumerable<int[]> distribute(int[] start, int target, int length)
+        {
+            var remaining = target - start.Sum();
+            if (length == start.Length - 1)
+            {
+                int[] array = start.ToArray();
+                array[length] = remaining;
+                yield return array;
+            }
+            else
+            {
+                for (int count = 0; count < remaining; count++)
+                {
+                    int[] array = start.ToArray();
+                    array[length] = count;
+                    foreach (var distribution in distribute(array, target, length + 1))
+                    {
+                        yield return distribution;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MVESIGN.NET.AdventOfCode/Day15/Ingredient.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace MVESIGN.NET.AdventOfCode.Day15
+{
+    /// <summary>
+    /// Class containing details of a cookie ingredient.
+    /// </summary>
+    public class Ingredient
+    {
+        /// <summary>
+        /// How many calories it adds to the cookie
+        /// </summary>
+        public long Calories { get; set; }
+
+        /// <summary>
+        /// How well it helps the cookie absorb milk.
+        /// </summary>
+        public long Capacity { get; set; }
+
+        /// <summary>
+        /// How well it keeps the cookie intact when full of milk.
+        /// </summary>
+        public long Durability { get; set; }
+
+        /// <summary>
+        /// How tasty it makes the cookie.
+        /// </summary>
+        public long Flavor { get; set; }
+        
+        /// <summary>
+        /// Score of the ingredient.
+        /// </summary>
+        public long Score
+        {
+            get
+            {
+                return Math.Max(0, Capacity) * Math.Max(0, Texture) * Math.Max(0, Flavor) * Math.Max(0, Durability);
+            }
+        }
+
+        /// <summary>
+        /// How it improves the feel of the cookie.
+        /// </summary>
+        public long Texture { get; set; }
+
+        /// <summary>
+        /// Append two ingredients together.
+        /// </summary>
+        /// <param name="first">Details of the first ingredient.</param>
+        /// <param name="second">Details of the second ingredient.</param>
+        /// <returns>Returns the calculates ingredient.</returns>
+        public static Ingredient operator +(Ingredient first, Ingredient second)
+        {
+            return new Ingredient()
+            {
+                Capacity = first.Capacity + second.Capacity,
+                Calories = first.Calories + second.Calories,
+                Durability = first.Durability + second.Durability,
+                Flavor = first.Flavor + second.Flavor,
+                Texture = first.Texture + second.Texture
+            };
+        }
+
+        /// <summary>
+        /// Multiple an ingredients with a number of teaspoons.
+        /// </summary>
+        /// <param name="first">Details of the ingredient.</param>
+        /// <param name="number">Number of teaspoons.</param>
+        /// <returns>Returns the calculates ingredient.</returns>
+        public static Ingredient operator *(Ingredient ingredient, int number)
+        {
+            return new Ingredient()
+            {
+                Capacity = ingredient.Capacity * number,
+                Calories = ingredient.Calories * number,
+                Durability = ingredient.Durability * number,
+                Flavor = ingredient.Flavor * number,
+                Texture = ingredient.Texture * number
+            };
+        }
+    }
+}
